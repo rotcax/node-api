2133c46b19d3a161314c4bdcf48b8e26a8cdda1f
Victor Caceres <caceres.vc95@gmail.com>
2020-09-17 22:37:48 -0400

    add cors

--- src/config/express.js
+++ src/config/express.js
@@ -1,12 +1,14 @@
 'use strict';
 
 const express     = require('express');
+const cors        = require('cors');
 const routes      = require('../routes');
 const bodyParser  = require('body-parser');
 
 const initServer = () => {
   const app = express();
 
+  app.use(cors());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
 
